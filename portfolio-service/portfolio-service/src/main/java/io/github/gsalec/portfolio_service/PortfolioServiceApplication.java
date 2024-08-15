6bdd59162d926468e4074452a6825ab12bce3099
gsalesc <gsalescosta29@gmail.com>
2024-08-14 21:09:32 -0300

    Adicionando portfolio-service e user-service a workspace

---
+++ portfolio-service/portfolio-service/src/main/java/io/github/gsalec/portfolio_service/PortfolioServiceApplication.java
@@ -0,0 +1,13 @@
+package io.github.gsalec.portfolio_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioServiceApplication.class, args);
+	}
+
+}
