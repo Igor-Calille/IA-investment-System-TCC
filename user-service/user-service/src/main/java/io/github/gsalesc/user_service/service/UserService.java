de5cbdffdb26702f3105b5fa3cc655e84096c5ba
gsalesc <gsalescosta29@gmail.com>
2024-08-20 19:11:12 -0300

    feat: Configurando o banco de dados

--- user-service/user-service/src/main/java/io/github/gsalesc/user_service/service/UserService.java
+++ user-service/user-service/src/main/java/io/github/gsalesc/user_service/service/UserService.java
@@ -1,5 +1,18 @@
 package io.github.gsalesc.user_service.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import io.github.gsalesc.user_service.repository.UserRepository;
+
+@Service
 public class UserService {
+
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+
+
 
 }
